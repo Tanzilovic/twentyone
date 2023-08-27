3189e2ddd9a93e94a46c8a04d5af162dfbde0736
Tanzilovic <133254875+Tanzilovic@users.noreply.github.com>
2023-08-27 11:38:55 +0100

    Update Cargo.toml for publish

---
+++ twentyone/target/debug/deps/twenty_one_dares-0aedaa5a4f33231f.d
@@ -0,0 +1,5 @@
+C:\Users\kisim\OneDrive\Desktop\git_repos\twentyone\twentyone\twentyone\target\debug\deps\twenty_one_dares-0aedaa5a4f33231f.rmeta: src\main.rs
+
+C:\Users\kisim\OneDrive\Desktop\git_repos\twentyone\twentyone\twentyone\target\debug\deps\twenty_one_dares-0aedaa5a4f33231f.d: src\main.rs
+
+src\main.rs:
